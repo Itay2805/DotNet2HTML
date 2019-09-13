8233d5ef820ea6d5da5025c5643599a5df94021f
itay2805 <itay2805@protonmail.com>
2019-09-14 01:18:00 +0300

    Added bunch of attribute tests

---
+++ DotNet2HTML.Test/Attributes/AttrShortFormTest.cs
@@ -0,0 +1,84 @@
+﻿using DotNet2HTML.Attributes;
+using DotNet2HTML.Tags;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using static DotNet2HTML.TagCreator;
+
+namespace DotNet2HTML.Test.Attributes
+{
+
+    [TestClass]
+    public class AttrShortFormTest
+    {
+
+        [TestMethod]
+        public void AddTo_EmptyTag()
+        {
+            string expected = "<input id=\"some-id\" class=\"some-class\">";
+            string actual = Input(Attrs("#some-id.some-class")).Render();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTo_ContainerTag()
+        {
+            string expected = "<div id=\"some-id\" class=\"some-class\"></div>";
+            string actual = Div(Attrs("#some-id.some-class")).Render();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTo_JustId()
+        {
+            string expected = "<div id=\"some-id\"></div>";
+            string actual = Div(Attrs("#some-id")).Render();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTo_JustClass()
+        {
+            string expected = "<div class=\"some-class\"></div>";
+            string actual = Div(Attrs(".some-class")).Render();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTo_AddTwoClasses()
+        {
+            string expected = "<div class=\"some-class some-other-class\"></div>";
+            string actual = Div(Attrs(".some-class.some-other-class")).Render();
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void AddTo_StringWithoutIdOrClass()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                Div(Attrs("some-class")).Render();
+            });
+        }
+
+        [TestMethod]
+        public void AddTo_StringWithTwoIds()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                Div(Attrs("#id1#id2")).Render();
+            });
+        }
+
+        [TestMethod]
+        public void AddTo_StringWithStupidlyPlacedId()
+        {
+            Assert.ThrowsException<InvalidOperationException>(() =>
+            {
+                Div(Attrs("id1#id2")).Render();
+            });
+        }
+    }
+}
---
+++ DotNet2HTML.Test/Attributes/AttributeTest.cs
@@ -0,0 +1,40 @@
+﻿using DotNet2HTML.Attributes;
+using DotNet2HTML.Tags;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+using static DotNet2HTML.TagCreator;
+
+namespace DotNet2HTML.Test.Attributes
+{
+
+    [TestClass]
+    public class AttributeTest
+    {
+
+        [TestMethod]
+        public void TestRender()
+        {
+            HtmlAttribute attributeWithValue = new HtmlAttribute("href", "http://example.com");
+            Assert.AreEqual(" href=\"http://example.com\"", attributeWithValue.Render());
+
+            HtmlAttribute attribute = new HtmlAttribute("required", null);
+            Assert.AreEqual(" required", attribute.Render());
+
+            HtmlAttribute nullAttribute = new HtmlAttribute(null, null);
+            Assert.AreEqual("", nullAttribute.Render());
+        }
+
+        [TestMethod]
+        public void TestSetAttribute()
+        {
+            ContainerTag testTag = new ContainerTag("a");
+            testTag.Attr("href", "http://example.com");
+            testTag.Attr("href", "http://example.org");
+            Assert.AreEqual("<a href=\"http://example.org\"></a>", testTag.Render());
+        }
+
+    }
+}
